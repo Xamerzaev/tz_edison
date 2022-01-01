a570f60680516417e4aab008e31ddefdd7964547
Xammirzaev <ramzanxam95reg@gmail.com>
2022-01-01 19:15:10 +0300

    ffffffff

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ import os
 import django_heroku
 
 django_heroku.settings(locals())
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -129,7 +129,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
